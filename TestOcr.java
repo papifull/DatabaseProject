31d0bc91ede9bc37271f391e1877b9ace73f118e
Suzycool <mbengueraymonde@gmail.com>
2021-06-07 17:50:54 +0000

    testocr

---
+++ TestOcr.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class TestOcr {
+	public static void main(String [] args) {
+		Ocr test = new Ocr(); 
+		test.RecupOcr();
+		System.out.println("");
+		
+		
+}
+
+}
+
+	
